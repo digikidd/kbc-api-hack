403ab61c79223a7a765f19b6f6fc07f6527b0959
Kelly Carmichael <digikidd@gmail.com>
2016-06-29 19:03:50 -0500

    no message

--- js/apiApp.js
+++ js/apiApp.js
@@ -1,3 +1,13 @@
 /**
  * Created by kellycarmichael on 6/29/16.
  */
+//open weather api key: 5b899b2dfcd111486e801f05f6530611
+
+var APIKEY = "5b899b2dfcd111486e801f05f6530611";
+
+var dayName = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+var monthName = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+
+
+
+
