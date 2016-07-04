434b0a647a69704aba7efe9c200a0b5688854265
Kelly Carmichael <digikidd@gmail.com>
2016-07-03 20:45:51 -0500

    no message

--- js/apiApp.js
+++ js/apiApp.js
@@ -1,13 +1,59 @@
 /**
  * Created by kellycarmichael on 6/29/16.
  */
-//open weather api key: 5b899b2dfcd111486e801f05f6530611
+$(document).ready(function () {
 
-var APIKEY = "5b899b2dfcd111486e801f05f6530611";
+    var tempUnit = false;
+    //function to evaluate
+    $('.tabs').click(function () {
+        if($('#farenheit').css('display') == 'none')
+        {
+           tempUnit = true;
+            console.log("farenheit is hidden");
+        }
+        else {
+            console.log("celsius is hidden");
+        }
+    });
 
-var dayName = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
-var monthName = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+    //Getting user input and making AJAX GET.
+    $('.waves-effect').click(function () {
+        var tempFarenheit = "&units=imperial";
+        var tempCelsius = "&units=metric";
+        var title = $('.titleText').val ();
+        var completeUrl = "";
+        var baseURL = "http://api.openweathermap.org/data/2.5/weather?q=";
+        var APIKEY = "&APPID=5b899b2dfcd111486e801f05f6530611";
+        if (tempUnit) {
+            completeUrl = baseURL + title + tempCelsius + APIKEY;
+        }
+        else {
+            completeUrl = baseURL + title + tempFarenheit + APIKEY;
+        }
+        //var dayName = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        //var monthName = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        //AJAX GET in jQuery
+        $.getJSON(completeUrl,function(data){
+            var weatherArray = data.weather.main;
 
+            //main.humidity
+            //main.temp
+            //
+            //
+            //
+            //
+            $('body').append('<div class="container"></div>');
+            $('.container').append('<div class="row"></div>');
+            $('.row').append('<div class="col s6">' + data.weather.main + '</div>');
+            $('.row').append('<div class="col s6"></div>');
+
+
+            //$('.film').last().append('<p>' + data.Country + '</p>');
+        });
+    });
+
+
+});
 
 
 
---
+++ js/test.js
@@ -0,0 +1,4 @@
+/**
+ * Created by kellycarmichael on 7/3/16.
+ */
+{"coord":{"lon":-97.74,"lat":30.27},"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04d"}],"base":"cmc stations","main":{"temp":306.35,"pressure":1015,"humidity":55,"temp_min":304.82,"temp_max":308.15},"wind":{"speed":3.6,"deg":200,"gust":8.2},"clouds":{"all":75},"dt":1467570613,"sys":{"type":1,"id":2558,"message":0.0031,"country":"US","sunrise":1467545652,"sunset":1467596195},"id":4671654,"name":"Austin","cod":200}
